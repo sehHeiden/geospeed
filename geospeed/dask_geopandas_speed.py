d552b06d9cd1ecde7db0e79828d4e7fa36d4b195
Sebastian Heiden <heiden-sebastian@t-online.de>
2024-09-19 21:29:16 +0200

    Optimize speed

--- geospeed/dask_geopandas_speed.py
+++ geospeed/dask_geopandas_speed.py
@@ -20,17 +20,17 @@ parcels_path = [x for directory in Path("../ALKIS").iterdir() for x in directory
 
 buildings_gdf = gpd.GeoDataFrame(pd.concat([gpd.read_file(x, engine="pyogrio", use_arrow=True) for x in buildings_path]))
 parcels_gdf = gpd.GeoDataFrame(pd.concat([gpd.read_file(x, engine="pyogrio", use_arrow=True) for x in parcels_path]))
-parcels_ddf = dpd.from_geopandas(buildings_gdf, npartitions=14)
+parcels_ddf = dpd.from_geopandas(parcels_gdf, npartitions=14)
 
-print(f"Loading data duration: {(time.time() - start):.2f} s.")
+print(f"Dask: Loading data duration: {(time.time() - start):.2f} s.")
 
 start_intersection = time.time()
 # Use Dask's map_partitions to apply the overlay function
 buildings_with_parcels = parcels_ddf.map_partitions(overlay_partitions, buildings_gdf).compute()
-print(f"Intersection takes: {(time.time() - start_intersection):.2f} s.")
+print(f"Dask: Intersection takes: {(time.time() - start_intersection):.2f} s.")
 
 start_saving = time.time()
 buildings_with_parcels.to_parquet("buildings_with_parcels.geoparquet")
-print(f"Saving takes: {(time.time() - start_saving):.2f} s.")
+print(f"Dask: Saving takes: {(time.time() - start_saving):.2f} s.")
 
-print(f"Total duration: {(time.time() - start):.2f} s.")
+print(f"Dask: Total duration: {(time.time() - start):.2f} s.")
